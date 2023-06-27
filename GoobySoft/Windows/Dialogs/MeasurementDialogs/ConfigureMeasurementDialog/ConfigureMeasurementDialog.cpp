fa605208bd8de6c98da39d863720c8ab00e17518
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-06-27 23:28:22 +0200

    Done with ADL400

--- GoobySoft/Windows/Dialogs/MeasurementDialogs/ConfigureMeasurementDialog/ConfigureMeasurementDialog.cpp
+++ GoobySoft/Windows/Dialogs/MeasurementDialogs/ConfigureMeasurementDialog/ConfigureMeasurementDialog.cpp
@@ -9,25 +9,6 @@ void addChangeDeleteButtons(const char popUpIdAdd[], const char popUpIdChange[],
 	if (ImGui::Button("Delete")) { ImGui::OpenPopup(popUpIdDelete); }
 }
 
-
-void addDevice(std::vector<std::vector<std::string>> &table, const char tableId[], std::vector<std::string> &ports, std::vector<std::string> &labels, std::vector<std::vector<std::string>> &items, std::vector<std::string> &selectedItems, size_t *selectedRow) {
-	addChangeDeleteButtons("add", "change", "delete");
-	int howManyComboBoxes = (int) labels.size() - 1U; // The last element of labels is Display Name. We want that as an InputText box
-	if (Tools_Gui_CreateDialogs_showPopUpComboInputTextMultiDialogOKCancle("add", howManyComboBoxes, labels, items, selectedItems)) {
-		table.push_back(selectedItems);
-	}
-	// The header should not be removed
-	if (table.size() > 1) {
-		if (Tools_Gui_CreateDialogs_showPopUpComboInputTextMultiDialogOKCancle("change", howManyComboBoxes, labels, items, selectedItems)) {
-			table.at(*selectedRow) = selectedItems;
-		}
-		if (Tools_Gui_CreateDialogs_showPopUpConfirmDialogOKCancle("delete", "Do you want to delete?", nullptr)) {
-			table.erase(table.begin() + *selectedRow);
-		}
-	}
-	Tools_Gui_CreateItems_createTableSelectable(tableId, table, selectedRow);
-}
-
 void Windows_Dialogs_MeasurementDialogs_ConfigureMeasurementDialog_showConfigureMeasurementDialog(bool* configureMeasurement) {
 	// Display
 	if (ImGui::BeginPopupModal("Configure measurement", configureMeasurement)) {
@@ -44,12 +25,37 @@ void Windows_Dialogs_MeasurementDialogs_ConfigureMeasurementDialog_showConfigure
 			// Get the ports 
 			std::vector<std::string> ports = Tools_Hardware_USB_Protocols_Modbus_getPortsOfConnectedDevices();
 			if (ImGui::CollapsingHeader("ADL400")) {
+				// Table stuffs
 				std::vector<std::vector<std::string>>& configurationTable = Tools_Communications_Devices_ADL400_getConfigurationTable();
 				std::vector<std::string> labels = configurationTable.at(0);
 				std::vector<std::vector<std::string>> items = { ports, SLAVE_ADDRESSES_ADL400, FUNCTION_NAMES_ADL400 };
 				static std::vector<std::string> selectedItems(4); // ports, slave address, function names, display name(last element as InputText)
 				static size_t selectedRow = 0;
-				addDevice(configurationTable, "configurationTableADL400", ports, labels, items, selectedItems, &selectedRow);
+
+				// Add change delete button
+				addChangeDeleteButtons("addADL400", "changeADL400", "deleteADL400");
+
+				// If we pressed ADD button
+				int howManyComboBoxes = (int)labels.size() - 1U; // The last element of labels is Display Name. We want that as an InputText box
+				if (Tools_Gui_CreateDialogs_showPopUpComboInputTextMultiDialogOKCancle("addADL400", howManyComboBoxes, labels, items, selectedItems)) {
+					configurationTable.push_back(selectedItems);
+				}
+
+				// If we pressed either change or delete button. The header should not be removed
+				if (configurationTable.size() > 1) {
+					// Change row
+					if (Tools_Gui_CreateDialogs_showPopUpComboInputTextMultiDialogOKCancle("changeADL400", howManyComboBoxes, labels, items, selectedItems)) {
+						configurationTable.at(selectedRow) = selectedItems;
+					}
+
+					// Delete row
+					if (Tools_Gui_CreateDialogs_showPopUpConfirmDialogOKCancle("deleteADL400", "Do you want to delete?", nullptr)) {
+						configurationTable.erase(configurationTable.begin() + selectedRow);
+					}
+				}
+
+				// Create table
+				Tools_Gui_CreateItems_createTableSelectable("configurationTableADL400", configurationTable, &selectedRow);
 			}
 			// Add more...
 		}
