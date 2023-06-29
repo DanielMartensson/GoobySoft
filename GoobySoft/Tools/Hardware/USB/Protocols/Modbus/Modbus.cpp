951f4af62fa0be00aa4c7529aa0bd74ae5a12a1f
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-06-29 21:20:55 +0200

    Error handling in Modbus

--- GoobySoft/Tools/Hardware/USB/Protocols/Modbus/Modbus.cpp
+++ GoobySoft/Tools/Hardware/USB/Protocols/Modbus/Modbus.cpp
@@ -18,31 +18,16 @@ modbus_t* getDevice(const char port[]) {
 }
 
 bool Tools_Hardware_USB_Protocols_Modbus_isConnected(const char port[]) {
-	if (!modbusDeviceExist(port)) {
-		return false;
-	}
-	else {
-		boost::asio::io_service io;
-		try {
-			// Open and see if it's bussy
-			boost::asio::serial_port testPort(io, port);
-			testPort.close(); // It seems that it's not used by anyone
-			return false;
-			
-		}
-		catch (const boost::system::system_error& ex) {
-			if (boost::asio::error::no_permission == ex.code().value()) {
-				return true; // Device is bussy
-			}
-		}
-	}
-	return false;
+	return modbusDeviceExist(port);
 }
 
  bool Tools_Hardware_USB_Protocols_Modbus_readRegister(const char port[], const unsigned int registerAddress, const unsigned int numberOfRegisters, uint16_t registers[]) {
 	if (Tools_Hardware_USB_Protocols_Modbus_isConnected(port)) {
-		int registerCount = modbus_read_registers(getDevice(port), registerAddress, numberOfRegisters, registers);
-		return registerCount == numberOfRegisters ? true : false;
+		if (modbus_read_registers(getDevice(port), registerAddress, numberOfRegisters, registers) == numberOfRegisters) {
+			return true;
+		}
+		std::fprintf(stderr, "Modbus read register failed: %s\n", modbus_strerror(errno));
+		return false;
 	}
 	else {
 		return false;
@@ -68,8 +53,8 @@ bool Tools_Hardware_USB_Protocols_Modbus_closeConnection(const char port[]) {
 bool Tools_Hardware_USB_Protocols_Modbus_openConnection(const char port[], const unsigned int baudrate, const char parity, const unsigned int dataBit, const unsigned int stopBits){
 	// Connect to modbus
 	modbus_t* addDevice = modbus_new_rtu(port, baudrate, parity, dataBit, stopBits);
-	int status = modbus_connect(addDevice);
-	if (status == -1) {
+	if (modbus_connect(addDevice) == -1) {
+		std::fprintf(stderr, "Modbus connection failed: %s\n", modbus_strerror(errno));
 		modbus_free(addDevice);
 		return false;
 	}
@@ -83,8 +68,11 @@ bool Tools_Hardware_USB_Protocols_Modbus_setSlaveAddress(const char port[], cons
 	if (!modbusDeviceExist(port)) {
 		return false;
 	}
-	int status = modbus_set_slave(getDevice(port), slaveAddress);
-	return status == 0 ? true : false;
+	if (modbus_set_slave(getDevice(port), slaveAddress) == -1) {
+		std::fprintf(stderr, "Modbus set slave failed: %s\n", modbus_strerror(errno));
+		return false;
+	}
+	return true;
 }
 
 std::vector<std::string> Tools_Hardware_USB_Protocols_Modbus_getPortsOfConnectedDevices() {
