981775d6666f704444195a953333a60fee45b7e9
Daniel Mårtensson <daniel.martensson100@outlook.com>
2025-10-09 14:23:14 +0200

    Update installForUbuntuDebian.sh

--- installForUbuntuDebian.sh
+++ installForUbuntuDebian.sh
@@ -1,6 +1,6 @@
 sudo apt-get install build-essential cmake libsdl3-dev libopengl-dev libboost-all-dev libmysqlcppconn-dev wget git -y
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.26-1_all.deb
-sudo dpkg -i mysql-apt-config_0.8.26-1_all.deb
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.34-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.34-1_all.deb
 sudo apt-get update
 git clone https://github.com/DanielMartensson/GoobySoft.git
 cd GoobySoft
