bf80c3b4b71d1f888f25a5da13bf916ee8fe0329
Daniel Martensson <daniel.martensson100@outlook.com>
2025-10-19 12:18:02 +0200

    update

--- GoobySoft/Tools/Storage/Parameters/Parameters.h
+++ GoobySoft/Tools/Storage/Parameters/Parameters.h
@@ -1,7 +1,6 @@
 #ifndef Parameters_H
 #define Parameters_H
-#include "../../Software/Libraries/CControl/ccontrol.h"
-#include "../../Software/Libraries/OpenSAEJ1939/OpenSAEJ1939.h"
+#include "../../Software/Libraries/Libraries.h"
 
 #define MAX_PROTOCOLS 5					// How many protocols can be used, CDC, Modbus RTU, Modbus TCP etc..
 #define MAX_DEVICES 10					// How many devices per protocol
