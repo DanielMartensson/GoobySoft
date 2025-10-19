1e61f7fa5100db97e272f124b958d949fb35ab8d
Daniel Martensson <daniel.martensson100@outlook.com>
2025-10-19 12:11:36 +0200

    update

--- GoobySoft/Tools/Software/Libraries/Libraries.h
+++ GoobySoft/Tools/Software/Libraries/Libraries.h
@@ -7,7 +7,7 @@
 #include "ImGui/imgui.h"
 #include "ImGui/backends/imgui_impl_sdl3.h"
 #include "ImGui/backends/imgui_impl_opengl3.h"
-#include "CControl/src/CControl/ccontrol.h"
+#include "CControl/Src/CControl/ccontrol.h"
 #include "modbus/modbus.h"
 #include "Open-SAE-J1939/Src/Open_SAE_J1939/Open_SAE_J1939.h"
 #include "conversions/conversions.h"
